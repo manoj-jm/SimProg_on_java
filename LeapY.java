9f3ad25e36b0e561e967295a171bf313c3870b60
manoj <manojjmvr49@gmail.com>
2024-03-24 19:53:50 +0530

    leap year

---
+++ LeapY.java
@@ -0,0 +1,10 @@
+public class LeapY {
+  public static void main(String[] args) {
+    int year = 2023;
+    if (year % 4 == 0 && !(year%100 ==0)) {
+      System.out.println(year + " is a leap year ");
+    }else{
+      System.out.println(year + " is not a leap year");
+    }
+  }
+}
