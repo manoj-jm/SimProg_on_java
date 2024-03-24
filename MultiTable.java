5082c61bf8e7e533597f95853eb13d294e6efddd
manoj <manojjmvr49@gmail.com>
2024-03-24 20:37:52 +0530

    multiplication table

---
+++ MultiTable.java
@@ -0,0 +1,8 @@
+public class MultiTable {
+  public static void main(String[] args) {
+    int n = 7;
+    for (int i = 1; i <= 10; i++) {
+      System.out.println(n + "*" + i + "=" + i * n);
+    }
+  }
+}
